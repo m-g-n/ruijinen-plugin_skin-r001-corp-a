40378af91c698b19a18f3be9c817d6ba45b6af5c
アクツユミ <akutsu@m-g-n.me>
2022-03-11 12:46:48 +0900

    正規版にバージョンアップ

--- ruijinen-skin-r001-corp-a.php
+++ ruijinen-skin-r001-corp-a.php
@@ -2,7 +2,7 @@
 /**
  * Plugin name: 類人猿 企業サイト向けパターン スキンA
  * Description: 企業サイト向けパターンに合ったスキンです
- * Version: 0.0.0.0.2
+ * Version: 0.0.0.1
  * Tested up to: 5.9
  * Requires at least: 5.9
  * Requires PHP: 5.6
@@ -16,8 +16,6 @@
  * @author mgn
  * @license GPL-2.0+
  */
-
-//TODO: 各ファイルの翻訳を作る
 
 namespace Ruijinen\Skin\R001_CORP_A;
 
